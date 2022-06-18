7ba255bfb9f3214f41438df2b32bfcf8d3ab7aeb
Alexandre Papin <me@alexandrepapin.name>
2022-06-18 21:03:19 +0200

    fixing import

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react";
-import { ObjectType } from "~/components/utils";
+import { ObjectType } from "~/util/utils";
 
 export function Header(props: { active: ObjectType }) {
   return (
